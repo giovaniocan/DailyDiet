7743b2c78ae86390da99bcc3dbc47919a3790ad7
giovani <giovaniocan@gmail.com>
2023-08-09 14:39:02 -0300

    fixed styled components importation

---
+++ src/theme/index.tsx
@@ -0,0 +1,35 @@
+export default {
+    COLORS: {
+      RED_DARK: '#BF3B44',
+      RED_MID: '#F3BABD',
+      RED_LIGHT: '#F4E6E7',
+      GREEN_DARK: '#639339',
+      GREEN_MID: '#CBE4B4',
+      GREEN_LIGHT: '#E5F0DB',
+  
+      GRAY_100: '#1B1D1E',
+      GRAY_200: '#333638',
+      GRAY_300: '#5C6265',
+      GRAY_400: '#B9BBBC',
+      GRAY_500: '#DDDEDF',
+      GRAY_600: '#DDDEDF',
+      GRAY_700: '#FAFAFA',
+      white: '#FFF',
+    },
+    FONT_FAMILY: {
+      BOLD: 'NunitoSans_700Bold',
+      REGULAR: 'NunitoSans_400Regular',
+    },
+    FONT_SIZE: {
+      SM: 12,
+      MD: 14,
+      LG: 16,
+      XG: 18,
+      '2XG': 24,
+      '3XG': 32,
+    },
+    LINE_HEIGHT: {
+      BASE: '41.6px',
+      SMALL: '18.2px',
+    },
+  }
